f0ac9fd4c8c8545110bea5db089891c288591b0a
sankar mukesh <ysankarmukesh@gmail.com>
2021-12-22 08:25:34 +0530

    just practice one

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/du3tfu1fb/image/upload",                // Cloudinary URL from the cloudinary dashboard
+    }
+}
+process.env.CLOUDINARY_URL
