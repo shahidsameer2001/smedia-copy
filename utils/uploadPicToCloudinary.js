193812cd1b529984cbf4666c74a7bb4e7c97a2af
sankar <47314238+sankarroyals@users.noreply.github.com>
2021-12-22 08:29:36 +0530

    Update uploadPicToCloudinary.js

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const uploadPic = async media => {
+const uploadPic = async (media) => {
   try {
     const form = new FormData();
-    form.append("file", media);
-    form.append("upload_preset", "social_media_app");
-    form.append("cloud_name", "indersingh");
-
-    const res = await axios.post(process.env.CLOUDINARY_URL, form);
+    form.append('file', media);
+    form.append('upload_preset', 'Social_media');
+    form.append('cloud_name', 'du3tfu1fb');
+    const res = await axios.post(process.env.CLOUDINARY_URL, form)
     return res.data.url;
-  } catch (error) {
-    return;
   }
-};
+  catch (err) {
+    console.log(err);
+  }
 
+}
 export default uploadPic;
